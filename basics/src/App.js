17ffc53a50108d29f4424d1a2d9bfd8f729ac8cc
aimansheikh17 <aimansheikh200117@gmail.com>
2024-12-26 12:51:12 +0530

    app made changes

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,7 +1,21 @@
+import About from "./About";
+import Contact from "./Contact";
+import Greeting from "./Greeting";
+import Header from "./Header";
+import Home from "./Home";
 
 function App() {
   return (
-   <h2>Hi</h2>
+    <div><h2>Hello from React js</h2>
+   {/*<Header/>
+   <Home/>
+   <About/>
+   <Contact/>*/}
+   <Greeting name = {'Aiman'}/>
+   <Greeting name = {'Almaz'}/>
+   </div>
+   
+    
   );
 }
 
---
+++ basics/src/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+    return(
+        <div>
+            <h1>Header content</h1>
+            <header>some header</header>
+        </div>
+         
+    )
+
+}
+export default Header;
