36c58aeb452c140b42df52f4f3abb4247af82871
aimansheikh17 <aimansheikh200117@gmail.com>
2024-12-26 13:56:49 +0530

    app made changes

---
+++ learnstate/src/App.js
@@ -0,0 +1,12 @@
+//import React, { useState } from "react";
+import Counter from "./Counter";
+
+export default function App() {
+  
+  return (
+    <div>
+       
+      <Counter/>
+    </div>
+  )
+}
---
+++ learnstate/src/Counter.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+export default function Counter() {
+    //let count = 0;
+    const [count, setCount] = useState(0);
+
+    const increase = () => setCount(count+1);
+
+    const decrease = () => setCount(count-1);
+
+    const reset = () => setCount(0);
+
+    return (
+        <div>
+            <h1>{count}</h1>
+            <button onClick={increase}>+</button>
+            <button onClick={decrease}>-</button>
+            <br></br><br></br>
+            <button onClick={reset}>Reset</button>
+        </div>
+    )
+}
